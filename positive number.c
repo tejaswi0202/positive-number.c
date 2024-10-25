eadc548c2c9f3944490b52180906611d90e6bc86
tejaswi0202 <ejjutejaswi@gmail.com>
2024-10-25 11:53:44 +0530

    positive number.c

---
+++ positive number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("enter n");
+    scanf("%d",&n);
+    if(n>0)
+    {
+        printf("positve");
+    }
+    else
+    {
+        printf("negative");
+    }
+}
