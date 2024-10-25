79b5a96a1fcc327dcfa07a3d202e28e815e1d36c
tejaswi0202 <ejjutejaswi@gmail.com>
2024-10-25 11:57:24 +0530

    zero.c

---
+++ zero.c
@@ -0,0 +1,19 @@
+void main()
+{
+    int n;
+    printf("enter n");
+    scanf("%d",&n);
+    if(n>0)
+    {
+        printf("positve");
+    }
+    else if(n==0)
+    {
+        printf("zero");
+        
+    }
+    else
+    {
+        printf("negative");
+    }
+}
