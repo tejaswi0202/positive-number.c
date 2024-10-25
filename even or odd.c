55e66140317ab7320bc9bab4c93b1fcd07c5c4fe
tejaswi0202 <ejjutejaswi@gmail.com>
2024-10-25 12:06:35 +0530

    even or odd.c

---
+++ even or odd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("enter n");
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+}
